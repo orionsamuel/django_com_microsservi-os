7e4ec155436a8c0cfe5dd1748d9d209b059e56b9
Samuel Oliveira <s.oliveira.15@hotmail.com>
2022-01-06 14:40:33 -0300

    Adicionando mais modelos ao models da pizzaria

--- tizza/pizza/models.py
+++ tizza/pizza/models.py
@@ -1,10 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Pizzeria(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=512)
+    phone = models.CharField(max_length=40)
+
 
 class Pizza(models.Model):
     title = models.CharField(max_length=120)
     description = models.CharField(max_length=120)
+    thumbnail_url = models.URLField()
+    approved = models.BooleanField(default=False)
+    creator = models.ForeignKey(Pizzeria, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'Pizza'
         verbose_name_plural = 'Pizzas'
         ordering = ['title']
+
+
+class Likes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
